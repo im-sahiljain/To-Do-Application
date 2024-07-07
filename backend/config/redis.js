b8ed6b28488a2c1104b03b6285ea102ba9f8fd52
im-sahiljain <mr.sahiljain14@gmail.com>
2024-07-07 14:28:12 +0530

    redis configuration

--- backend/config/redis.js
+++ backend/config/redis.js
@@ -1,18 +1,16 @@
 const redis = require('redis');
 
-// Create Redis client
 const client = redis.createClient({
-  host: 'localhost', // Redis server host
-  port: 6379,        // Redis server port
-  // Add more configuration options if needed
+  host: 'localhost', 
+  port: 6379,        
+ 
 });
 
-// Handle connection errors
 client.on('error', (err) => {
   console.error('Redis error:', err);
 });
 
-// Optional: Handle connect event
+
 client.on('connect', () => {
   console.log('Redis client connected');
 });
