6da6ffc9c3f3eb33b2dfd63460294d6180dacb38
im-sahiljain <mr.sahiljain14@gmail.com>
2024-07-07 15:54:21 +0530

    Handling the user registeration and login methods

---
+++ backend/controllers/authController.js
@@ -0,0 +1,38 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+
+exports.register = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findByEmail(email);
+    if (user) {
+      return res.status(400).json({ msg: 'User already exists' });
+    }
+    const userId = await User.create(email, password);
+    const token = jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: '1h' });
+    res.json({ token });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
+};
+
+exports.login = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findByEmail(email);
+    if (!user) {
+      return res.status(400).json({ msg: 'Invalid credentials' });
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ msg: 'Invalid credentials' });
+    }
+    const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+    res.json({ token });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
+};
